00a9206b29bbf5dd9f00567a119237105c57657d
Jan <72294368+frackowiak147@users.noreply.github.com>
2023-06-02 16:59:08 +0200

    Add files via upload

---
+++ book_reservation.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="navbar_sides">
+            <div class="navbar_cell" id="logo">
+                <img src="./img/logo.png">
+            </div>
+            <div class="navbar_cell" id="part_lib_name">
+                Biblioteka im. Juliusza Kobrzyńskiego
+            </div>
+            <div class="navbar_cell" id="full_lib_name">
+                Biblioteka im. Juliusza Kobrzyńskiego w Zamościu
+            </div>
+        </div>
+        <div class="navbar_sides right">
+            <div class="navbar_cell">
+                <div class="navigation">
+                    <a href="">KATALOG</a>
+                </div>
+            </div>
+            <div class="navbar_cell dropdown">
+                <div class="navigation">
+                    KONTO
+                    <div class="dropdown_content">
+                        <a href="login_form.php">WYLOGUJ</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="under_nav_space"></div>
+    <div> <a href="register_form.php">
+            <img src="./img/sherlock.jpg" alt="Sherlock" style="width:200px;height:300px;"></a> </div>
+</body>
+
+</html>
