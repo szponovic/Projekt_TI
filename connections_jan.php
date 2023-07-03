42650c793b0683fe2316a39121274c531f5bd9d2
Jan Frąckowiak <frackowiak147@gmail.com>
2023-07-03 19:43:47 +0200

    new file

---
+++ connections_jan.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost"; // Replace with the appropriate hostname or IP address
+$username = "root"; // Replace with your phpMyAdmin username
+$password = ""; // Replace with your phpMyAdmin password
+$database = "my_base"; // Replace with the name of your database
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+
+//echo "Connected successfully";
+
+?>
